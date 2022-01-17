4ae6169b52d070060f55a1a9d96a3a364bddea08
kohei-hagihara <tatnatt00@gmail.com>
2022-01-17 13:12:40 +0900

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,50 @@
+<template>
+  <div id="app">
+    <a>現在{{ number }}回クリックされています</a>
+    <button @click="countUp">カウントアップ</button>
+    <!--<input type="text" v-model="message">
+    <h1>{{message}}</h1>
+    -->
+  </div>
+</template>
+
+<script>
+// import HelloWorld from "./components/HelloWorld";
+
+export default {
+  name: "App",
+  components: {
+    // HelloWorld
+  },
+  data() {
+    return {
+      number: 0,
+      //message: 'こんにちは',
+      //@24
+    };
+  },
+  methods: {
+    countUp: function () {
+      this.number += 1;
+      this.getIp();
+    },
+    getIp() {
+      this.axios
+        .get("http://192.168.1.76:9898/")
+        .then((response) => console.log(response.data))
+        .catch((error) => console.log(error));
+    },
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
